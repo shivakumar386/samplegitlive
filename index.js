03f0ba239d6cbe9b90b37d717dbede6936f1f4ef
Rajeshsingarapu <singarapu.rajesh2@gmail.com>
2022-11-22 18:17:23 +0530

    rajesh

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 console.log("Hello Shiva....");
 console.log("Edited by shiva....");
-for(let i = 0; i < i.length; i++) {
+for (let i = 0; i < i.length; i++) {
     console.log(i)
 }
-for(let i = 0; i < i.length; i++) {
+for (let i = 0; i < i.length; i++) {
     console.log(i)
 }
+
